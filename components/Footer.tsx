925f16c3c0b40f5ab462d799646df97fe388f6c7
Mikeya74 <michael.anderson.74@gmail.com>
2026-06-07 17:01:47 -0600

    fix logo spacing

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,7 @@ export function Footer({ teamCode }: { teamCode: string }) {
   if (!logoUrl) return null;
 
   return (
-    <div style={{ position:"fixed", bottom:0, left:0, right:0, padding:"12px 20px", display:"flex", alignItems:"center", justifyContent:"center", zIndex:100, pointerEvents:"none" }}>
+    <div style={{ position:"fixed", bottom:8, left:0, right:0, padding:"12px 20px", display:"flex", alignItems:"center", justifyContent:"center", zIndex:100, pointerEvents:"none" }}>
       <img src={logoUrl} alt="Logo" style={{ height:32, objectFit:"contain", maxWidth:160, opacity:0.85 }} />
     </div>
   );
